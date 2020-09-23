9a98e4fe1b3660a07b11dcea3d2d01715ef43a9f
yzhao <295881455@qq.com>
2020-09-23 15:34:15 +0800

    v1.0

---
+++ precache-manifest.5105673a4528f51e0b310381243c3e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a8bac33496585ad93c567ab05d3eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c28e7bc011c37e93b0",
+    "url": "/static/css/main.cebe2ce0.chunk.css"
+  },
+  {
+    "revision": "3152a35a4acb3e4aa530",
+    "url": "/static/js/2.0e0fcb2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e0fcb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c28e7bc011c37e93b0",
+    "url": "/static/js/main.ad0701dd.chunk.js"
+  },
+  {
+    "revision": "663f4b47d7f87a36ab08",
+    "url": "/static/js/runtime-main.d7bd2fb3.js"
+  }
+]);
